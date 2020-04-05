c7268185a3a8d87fb9faeb3e86a2c9c93f7b8976
Greg Hale <imalsogreg@gmail.com>
2020-04-04 22:06:31 -0400

    minor cleanup

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,17 @@ use super::state;
 use super::params;
 
 
+/// Load initial rust state from a dhall expression
+pub fn load_state(s : &str) -> Result<state::State, serde_json::Error> {
+    load_dhall_expr::<state::State>(s)
+}
+
+/// Load simulation parameters from a dhall expression
+pub fn load_params(s : &str) -> Result<params::Params, serde_json::Error> {
+    load_dhall_expr::<params::Params>(s)
+}
+
+
 pub fn load_dhall_expr<T>(s : &str) -> Result<T, serde_json::Error>
 where T: serde::de::DeserializeOwned
 {
@@ -23,63 +34,3 @@ where T: serde::de::DeserializeOwned
 
     serde_json::from_str(data)
 }
-
-pub fn load_dhall_expr2<T>(s : & str) -> Result<T, serde_json::Error>
-    where T: serde::de::DeserializeOwned
-{
-    println!("spawn");
-    let mut dhall_child =
-        process::Command::new("dhall-to-json")
-        .stdin(process::Stdio::piped())
-        .stdout(process::Stdio::piped())
-        .spawn()
-        .expect("Could not spawn dhall-to-json");
-
-    println!("write_all");
-    dhall_child.stdin
-               .as_mut()
-               .unwrap()
-               .write_all(s.as_bytes()).expect("Failed to write s");
-
-    let mut data = String::new();
-
-    println!("read_to_string");
-    dhall_child.stdout
-               .as_mut()
-               .unwrap()
-               .read_to_string(&mut data)
-               .expect("Failed to read from dhall-to-json");
-
-    println!("serde_json");
-    serde_json::from_str(data.as_str())
-}
-
-pub fn load_state(s : &str) -> Result<state::State, serde_json::Error> {
-    load_dhall_expr::<state::State>(s)
-}
-
-pub fn load_params(s : &str) -> Result<params::Params, serde_json::Error> {
-    load_dhall_expr::<params::Params>(s)
-}
-
-
-// pub fn load_dhall_state(f : &Path) -> Result<state::State, serde_json::Error> {
-//     let cat_child =
-//         process::Command::new("cat")
-//         .arg(f.to_str().expect("Could not convert path"))
-//         .stdout(process::Stdio::piped())
-//         .spawn()
-//         .expect("Failed to run cat");
-
-//     let cat_out = cat_child.stdout.expect("Failed to open cat stdout");
-
-//     let dhall_result =
-//         process::Command::new("dhall-to-json")
-//         .stdin(process::Stdio::from(cat_out))
-//         .stderr(process::Stdio::piped())
-//         .output()
-//         .expect("Could not spawn dhall-to-json");
-
-//   let data : &str = std::str::from_utf8(dhall_result.stdout.as_slice()).expect("got str");
-//   serde_json::from_str(data)
-// }
